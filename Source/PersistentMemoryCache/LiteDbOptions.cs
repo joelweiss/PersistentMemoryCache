732798971ca1a2028e56929e53612911a5561b88
Joel Weiss <joelweiss@outlook.com>
2016-11-20 17:53:46 -0500

    Check for null file name

--- Source/PersistentMemoryCache/LiteDbOptions.cs
+++ Source/PersistentMemoryCache/LiteDbOptions.cs
@@ -1,10 +1,12 @@
-﻿namespace PersistentMemoryCache
+﻿using System;
+
+namespace PersistentMemoryCache
 {
     public class LiteDbOptions
     {
         public LiteDbOptions(string fileName)
         {
-            FileName = fileName;
+            FileName = fileName ?? throw new ArgumentNullException(nameof(fileName));
         }
 
         public string FileName { get; }
