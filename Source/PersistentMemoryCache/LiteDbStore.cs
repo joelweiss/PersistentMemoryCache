71fc8cf7e3f9a9724027c8ead5fae2192f9e9ca7
Joel Weiss <joelweiss@outlook.com>
2017-06-19 16:25:50 -0400

    Updated: packages

--- Source/PersistentMemoryCache/LiteDbStore.cs
+++ Source/PersistentMemoryCache/LiteDbStore.cs
@@ -7,59 +7,60 @@ namespace PersistentMemoryCache
 {
     public class LiteDbStore : IPersistentStore
     {
-        private readonly string _FileName;
+        private const string _CollectionName = "PersistedCacheEntry";
+        private readonly string _ConnectionString;
 
         public LiteDbStore(LiteDbOptions options)
         {
-            _FileName = options.FileName;
-            using (var db = new PersistentLiteDatabase(_FileName))
+            _ConnectionString = $"filename={options.FileName};upgrade=true";
+            using (var db = new PersistentLiteDatabase(_ConnectionString))
             {
-                var collection = db.GetCollection<Internal.LiteDbCacheEntry>("PersistedCacheEntry");
+                var collection = db.GetCollection<Internal.LiteDbCacheEntry>(_CollectionName);
                 collection.EnsureIndex(pce => pce.CacheName);
             }
         }
 
         public int AddEntry(Internal.LiteDbCacheEntry entry)
         {
-            using (var db = new PersistentLiteDatabase(_FileName))
+            using (var db = new PersistentLiteDatabase(_ConnectionString))
             {
-                var collection = db.GetCollection<Internal.LiteDbCacheEntry>("PersistedCacheEntry");
+                var collection = db.GetCollection<Internal.LiteDbCacheEntry>(_CollectionName);
                 return collection.Insert(entry).AsInt32;
             }
         }
 
         public List<Internal.LiteDbCacheEntry> LoadEntries(string cacheName)
         {
-            using (var db = new PersistentLiteDatabase(_FileName))
+            using (var db = new PersistentLiteDatabase(_ConnectionString))
             {
-                var collection = db.GetCollection<Internal.LiteDbCacheEntry>("PersistedCacheEntry");
+                var collection = db.GetCollection<Internal.LiteDbCacheEntry>(_CollectionName);
                 return collection.Find(pce => pce.CacheName == cacheName).ToList();
             }
         }
 
         public Internal.LiteDbCacheEntry LoadEntry(int key)
         {
-            using (var db = new PersistentLiteDatabase(_FileName))
+            using (var db = new PersistentLiteDatabase(_ConnectionString))
             {
-                var collection = db.GetCollection<Internal.LiteDbCacheEntry>("PersistedCacheEntry");
+                var collection = db.GetCollection<Internal.LiteDbCacheEntry>(_CollectionName);
                 return collection.FindById(new BsonValue(key));
             }
         }
 
         public bool UpdateEntry(int key, Internal.LiteDbCacheEntry entry)
         {
-            using (var db = new PersistentLiteDatabase(_FileName))
+            using (var db = new PersistentLiteDatabase(_ConnectionString))
             {
-                var collection = db.GetCollection<Internal.LiteDbCacheEntry>("PersistedCacheEntry");
+                var collection = db.GetCollection<Internal.LiteDbCacheEntry>(_CollectionName);
                 return collection.Update(new BsonValue(key), entry);
             }
         }
 
         public void RemoveEntry(int id)
         {
-            using (var db = new LiteDatabase(_FileName))
+            using (var db = new LiteDatabase(_ConnectionString))
             {
-                var collection = db.GetCollection<Internal.LiteDbCacheEntry>("PersistedCacheEntry");
+                var collection = db.GetCollection<Internal.LiteDbCacheEntry>(_CollectionName);
                 collection.Delete(new BsonValue(id));
             }
         }
